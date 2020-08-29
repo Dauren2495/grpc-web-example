59934b678ef627eb5d01d4178ea24a4668e8d385
Serkeshev Dauren <dauren@stunitas.com>
2020-08-29 15:07:24 +0900

    add client code

---
+++ client/client.js
@@ -0,0 +1,12 @@
+const { AddRequest, AddResponse } = require("./calculator_pb")
+const { CalculatorClient } = require("./calculator_grpc_web_pb")
+var client = new CalculatorClient('http://localhost:8080');
+
+var request = new AddRequest()
+
+request.setNum1(2)
+request.setNum2(3)
+
+client.add(request, {}, (err, response) => {
+    console.log("response : ",response)
+})
